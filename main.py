760b0020d8c73e225da5eeccc8ec0bd4ebd296f4
Vitor Pereira Resende <vitorpereiraresende26@gmail.com>
2024-07-09 11:20:31 -0300

    games endpoints

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 import requests
 import os
 
@@ -18,7 +18,10 @@ app = FastAPI()
 app.add_exception_handler(ExceptionHandler, exception_handler)
 
 
-@app.get("/api/player/{username}")
+api_router = APIRouter(prefix="/api", tags=["api"])
+
+
+@api_router.get("/player/{username}")
 async def player(username: str):
     url = f"{API_URL}/pub/player/{username}"
     response = requests.get(url, headers=HEADERS)
@@ -28,3 +31,42 @@ async def player(username: str):
         return data
     elif response.status_code == 404:
         raise ExceptionHandler(status_code=404, message=f"User {username} not found")
+
+
+@api_router.get("/player/stats/{username}")
+async def stats(username: str):
+    url = f"{API_URL}/pub/player/{username}/stats"
+    response = requests.get(url, headers=HEADERS)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    elif response.status_code == 404:
+        raise ExceptionHandler(status_code=404, message=f"User {username} not found")
+
+
+@api_router.get("/player/games/{username}")
+async def games(username: str):
+    url = f"{API_URL}/pub/player/{username}/games"
+    response = requests.get(url, headers=HEADERS)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    elif response.status_code == 404:
+        raise ExceptionHandler(status_code=404, message=f"User {username} not found")
+
+
+@api_router.get("/player/games-archives/{username}")
+async def games_archives(username: str):
+    url = f"{API_URL}/pub/player/{username}/games/archives"
+    response = requests.get(url, headers=HEADERS)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    elif response.status_code == 404:
+        raise ExceptionHandler(status_code=404, message=f"User {username} not found")
+
+
+app.include_router(api_router)
